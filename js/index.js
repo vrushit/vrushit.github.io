f5fc7863b8f00f6ca74678f4d48d1bb1c68a7055
vrushit <bharuchavrushit@ymail.com>
2020-06-17 16:01:53 +0530

    Loading Website

--- js/index.js
+++ js/index.js
@@ -13,14 +13,24 @@
 //     display.style.display= 'inline';
 // });
 
-window.addEventListener('DOMContentLoaded', function() {
-    // your code here
-    const preload = document.querySelector('.preload');
-     preload.classList.add('preload-finish');
+// window.addEventListener('DOMContentLoaded', function() {
+//     // your code here
+//     const preload = document.querySelector('.preload');
+//      preload.classList.add('preload-finish');
 
      
 
- });
+//  });
 
- const display = document.querySelector(".display");
-     display.style.display= 'inline';
+//  const display = document.querySelector(".display");
+//  display.style.display= 'inline';
+
+$(window).ready(function(){
+
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+
+     const display = document.querySelector(".display");
+ display.style.display= 'inline';
+
+});
