a9ed97d1ddfef5b21b620e412290ddf807ff1725
vrushit <bharuchavrushit@ymail.com>
2020-06-17 16:47:50 +0530

    Loading Webpages Complete

--- js/index.js
+++ js/index.js
@@ -1,38 +1,12 @@
-// document.addEventListener("DOMContentLoaded", ()=>{
-//     alert("Windows Loaded");
-// });
-
-//Preloader
 
 window.addEventListener('load', () => {
 
     const preload = document.querySelector('.preload');
     preload.classList.add('preload-finish');
-    window.location = "LandingPage.html";
+
+    const display = document.querySelector(".display");
+    display.style.display= 'inline';
+    
 
 });
-
-
-
-
-// window.addEventListener('DOMContentLoaded', function() {
-//     // your code here
-//     const preload = document.querySelector('.preload');
-//      preload.classList.add('preload-finish');
-
-     
-
-//  });
-
-
-
-// $(window).ready(function(){
-
-//     const preload = document.querySelector('.preload');
-//     preload.classList.add('preload-finish');
-
-//      const display = document.querySelector(".display");
-//  display.style.display= 'inline';
-
-// });
 
