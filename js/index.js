f4cf11ee9cb6574b2596344e97edbffc2319b006
vrushit <bharuchavrushit@ymail.com>
2020-06-17 14:53:26 +0530

    Loding Complete

--- js/index.js
+++ js/index.js
@@ -8,8 +8,10 @@ window.addEventListener('load', () => {
 
     const preload = document.querySelector('.preload');
     preload.classList.add('preload-finish');
-   
+
     const display = document.querySelector(".display");
     display.style.display= 'inline';
 
+
 });
+
