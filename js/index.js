7d69f3d0ab866bbe1fae9acaf1421244708fc463
vrushit <bharuchavrushit@ymail.com>
2020-06-17 16:38:13 +0530

    Loadin

--- js/index.js
+++ js/index.js
@@ -8,14 +8,9 @@ window.addEventListener('load', () => {
 
     const preload = document.querySelector('.preload');
     preload.classList.add('preload-finish');
-    
-    const display = document.querySelector(".display");
-    display.style.display= 'inline';
+    window.location = "LandingPage.html";
 
-    
-   
 });
-
 
 
 
