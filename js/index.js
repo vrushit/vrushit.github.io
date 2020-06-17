03fdd3d31622f200949250c4821efddc1ede022c
vrushit <bharuchavrushit@ymail.com>
2020-06-17 14:09:32 +0530

    Loader

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     alert("Windows Loaded");
+// });
+
+//Preloader
+
+window.addEventListener('load', () => {
+
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+    // window.location = "index.html";
+
+});
