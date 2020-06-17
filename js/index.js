c7f029ebcbe0634cf5c2b65b503fd56873a2b533
vrushit <bharuchavrushit@ymail.com>
2020-06-17 14:23:11 +0530

    Loader1

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@
 
 window.addEventListener('load', () => {
 
-    const preload = document.querySelector('.preload');
-    preload.classList.add('preload-finish');
+    // const preload = document.querySelector('.preload');
+    // preload.classList.add('preload-finish');
     // window.location = "index.html";
+    const display = document.querySelector(".display");
+    display.style.display= 'inline';
 
 });
