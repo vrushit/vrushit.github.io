7d7dea8a1cb0633e6b739c5776769362307b7587
vrushit <bharuchavrushit@ymail.com>
2020-06-19 17:07:35 +0530

    Loader

--- js/index.js
+++ js/index.js
@@ -5,8 +5,26 @@ window.addEventListener('load', () => {
     preload.classList.add('preload-finish');
 
     const display = document.querySelector(".display");
-    display.style.display= 'inline';
+    display.style.display = "inline";
     
+    window.onload = function() {
+        //considering there aren't any hashes in the urls already
+        if(!window.location.hash) {
+            //setting window location
+            window.location = window.location + '#loaded';
+            //using reload() method to reload web page
+            window.location.reload();
+        }
+    }
+
 
 });
 
+
+
+
+// document.onreadystatechange = function(){
+//     if(document.readyState === 'complete'){
+//         location.reload();
+//     }
+// }
