b20e3bb491acec0074e0607e637d7ff9dbc55765
vrushit <bharuchavrushit@ymail.com>
2020-06-19 17:49:53 +0530

    Loader Commits

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 
+
+
 window.addEventListener('load', () => {
 
     const preload = document.querySelector('.preload');
@@ -8,21 +10,3 @@ window.addEventListener('load', () => {
     display.style.display = "inline";
     
 });
-
-window.onload = function() {
-    //considering there aren't any hashes in the urls already
-    if(!window.location.hash) {
-        //setting window location
-        window.location = window.location + '#loaded';
-        //using reload() method to reload web page
-        window.location.reload();
-    }
-}
-
-
-
-// document.onreadystatechange = function(){
-//     if(document.readyState === 'complete'){
-//         location.reload();
-//     }
-// }
