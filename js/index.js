9928382429cde1708b194f2144d201040b986520
vrushit <bharuchavrushit@ymail.com>
2020-06-19 22:49:26 +0530

    Loading Loops

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-// window.addEventListener('load', () => {
+window.addEventListener('load', () => {
 
-//     const preload = document.querySelector('.preload');
-//     preload.classList.add('preload-finish');
-
-//     const display = document.querySelector(".display");
     
-//     display.style.display = "inline";
-// });
+});
 
 
 $(window).ready(function() {
