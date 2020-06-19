02114d346d2945283280985205f75dd896c9cf40
vrushit <bharuchavrushit@ymail.com>
2020-06-19 22:32:55 +0530

    Loaders Component

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// window.addEventListener('load', () => {
+
+//     const preload = document.querySelector('.preload');
+//     preload.classList.add('preload-finish');
+
+//     const display = document.querySelector(".display");
+    
+//     display.style.display = "inline";
+// });
+
+
+$(document).ready(function() {
+
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+
+    const display = document.querySelector(".display");
+    display.style.display = "inline";
+    display.style.opacity = "1";
+});
